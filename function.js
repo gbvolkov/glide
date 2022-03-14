e549e74525baf236cd2332b5632f729b3bfd8dc0
Georgy <gbvolkov@gmail.com>
2022-03-14 12:19:19 +0100

    gv

--- function.js
+++ function.js
@@ -46,6 +46,6 @@ window.function = function (startdate, tzdir, timelag, routetime) {
 	timelag = timelag.value ?? "0:00";
 	routetime = routetime.value ?? "0:00";
 
-	return new Date(calcDateTime(startdate, tzdir, timelag, routetime));
+	return calcDateTime(startdate, tzdir, timelag, routetime);
   }
   
