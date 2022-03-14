3053a5a8a7428f33ec0b002520e9f01b6e13b3e8
Georgy <gbvolkov@gmail.com>
2022-03-14 10:16:46 +0100

    16

--- function.js
+++ function.js
@@ -15,27 +15,30 @@ function timeParse(timeInterval) {
 	return dt;
 }
 
-window.function = function (startdate, tzdir, timelagStr, routetimeStr) {
-	// For each parameter, its `.value` contains
-	// either its value in the type you've declared,
-	// or it's `undefined`.  This is a good place to
-	// extract the `.value`s and assign default
-	// values.
-	
+function calcDateTime(startdate, tzdir, timelagStr, routetimeStr) {
 	startdate = startdate.value ?? Date().now();
-	startdate = Date.parse(startdate);
 	tzdir = tzdir.value ?? 0;
 	timelagStr = timelagStr.value ?? "0:00";
 	timelag = Date(timeParse(timelagStr));
 	routetimeStr = routetimeStr.value ?? "0:00";
 	routetime = Date(timeParse(routetimeStr));
 
-	startdate = startdate + timelag + routetime;
+	return startdate + timelag + routetime;startdate = Date.parse(startdate);
+	
+}
+
+window.function = function (startdate, tzdir, timelagStr, routetimeStr) {
+	// For each parameter, its `.value` contains
+	// either its value in the type you've declared,
+	// or it's `undefined`.  This is a good place to
+	// extract the `.value`s and assign default
+	// values.
+
 	// Your function should return the exact type
 	// you've declared for the `result` in
 	// `glide.json`, or `undefined` if there's an
 	// error or no result can be produced, because a
 	// required input is `undefined`, for example.
-	return startdate;
+	return calcDateTime(startdate, tzdir, timelagStr, routetimeStr);
   }
   
